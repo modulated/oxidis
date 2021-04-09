c0cdea32e6f5c3e82c1c8ac3638e2d73ad1f340c
modulated <cowkingdom@hotmail.com>
2021-04-09 17:34:06 +1000

    logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::{env, net::SocketAddrV4};
+use pretty_env_logger;
+use log::*;
 use warp::{Filter, http::StatusCode};
 mod data;
 mod db;
@@ -6,13 +9,19 @@ mod handler;
 
 #[tokio::main]
 async fn main() {
+    env::set_var("RUST_LOG", "TRACE");
+    pretty_env_logger::init();
+    info!("Initializing server.");
     let health_route = warp::path!("health")
         .map(|| {
-            println!("Helathy.");
+            info!("Health check.");
             StatusCode::OK
         });
 
     let routes = health_route.with(warp::cors().allow_any_origin());
 
-    warp::serve(routes).run(([127,0,0,1], 8000)).await;
+    let addr = SocketAddrV4::new(std::net::Ipv4Addr::new(127, 0, 0, 1), 8000);
+    info!("Serving on {:?}.", addr);
+
+    warp::serve(routes).run(addr).await;
 }
